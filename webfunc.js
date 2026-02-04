da9ded6080e6194a2fae256857cddc8a66638ea3
Antonio-Icaro <antonio.icaro@aluno.uece.br>
2026-02-04 10:34:52 -0300

    Starting development

---
+++ webfunc.js
@@ -0,0 +1,286 @@
+
+var gl;
+var prog;
+var df = 2.0;
+
+var angle = 0;
+
+function getGL(canvas)
+{
+    var gl = canvas.getContext("webgl");
+    if(gl) return gl;
+    
+    gl = canvas.getContext("experimental-webgl");
+    if(gl) return gl;
+    
+    alert("Contexto WebGL inexistente! Troque de navegador!");
+    return false;
+}
+
+function createShader(gl, shaderType, shaderSrc)
+{
+	var shader = gl.createShader(shaderType);
+	gl.shaderSource(shader, shaderSrc);
+	gl.compileShader(shader);
+	
+	if(gl.getShaderParameter(shader, gl.COMPILE_STATUS))
+		return shader;
+	
+	alert("Erro de compilação: " + gl.getShaderInfoLog(shader));
+	
+	gl.deleteShader(shader);
+}
+
+function createProgram(gl, vtxShader, fragShader)
+{
+	var prog = gl.createProgram();
+	gl.attachShader(prog, vtxShader);
+	gl.attachShader(prog, fragShader);
+	gl.linkProgram(prog);
+	
+	if(gl.getProgramParameter(prog, gl.LINK_STATUS))
+		return prog;
+
+    alert("Erro de linkagem: " + gl.getProgramInfoLog(prog));
+	
+	gl.deleteProgram(prog);	
+}
+
+var modelData = null; 
+
+function parseOBJ(text) {
+  // because indices are base 1 let's just fill in the 0th data
+  const objPositions = [[0, 0, 0]];
+  const objTexcoords = [[0, 0]];
+  const objNormals = [[0, 0, 0]];
+
+  // same order as `f` indices
+  const objVertexData = [
+    objPositions,
+    objTexcoords,
+    objNormals,
+  ];
+
+  // same order as `f` indices
+  let webglVertexData = [
+    [],   // positions
+    [],   // texcoords
+    [],   // normals
+  ];
+
+  const materialLibs = [];
+  const geometries = [];
+  let geometry;
+  let groups = ['default'];
+  let material = 'default';
+  let object = 'default';
+
+  const noop = () => {};
+
+  function newGeometry() {
+    // If there is an existing geometry and it's
+    // not empty then start a new one.
+    if (geometry && geometry.data.position.length) {
+      geometry = undefined;
+    }
+  }
+
+  function setGeometry() {
+    if (!geometry) {
+      const position = [];
+      const texcoord = [];
+      const normal = [];
+      webglVertexData = [
+        position,
+        texcoord,
+        normal,
+      ];
+      geometry = {
+        object,
+        groups,
+        material,
+        data: {
+          position,
+          texcoord,
+          normal,
+        },
+      };
+      geometries.push(geometry);
+    }
+  }
+
+  function addVertex(vert) {
+    const ptn = vert.split('/');
+    ptn.forEach((objIndexStr, i) => {
+      if (!objIndexStr) {
+        return;
+      }
+      const objIndex = parseInt(objIndexStr);
+      const index = objIndex + (objIndex >= 0 ? 0 : objVertexData[i].length);
+      webglVertexData[i].push(...objVertexData[i][index]);
+    });
+  }
+
+  const keywords = {
+    v(parts) {
+      objPositions.push(parts.map(parseFloat));
+    },
+    vn(parts) {
+      objNormals.push(parts.map(parseFloat));
+    },
+    vt(parts) {
+      // should check for missing v and extra w?
+      objTexcoords.push(parts.map(parseFloat));
+    },
+    f(parts) {
+      setGeometry();
+      const numTriangles = parts.length - 2;
+      for (let tri = 0; tri < numTriangles; ++tri) {
+        addVertex(parts[0]);
+        addVertex(parts[tri + 1]);
+        addVertex(parts[tri + 2]);
+      }
+    },
+    s: noop,    // smoothing group
+    mtllib(parts, unparsedArgs) {
+      // the spec says there can be multiple filenames here
+      // but many exist with spaces in a single filename
+      materialLibs.push(unparsedArgs);
+    },
+    usemtl(parts, unparsedArgs) {
+      material = unparsedArgs;
+      newGeometry();
+    },
+    g(parts) {
+      groups = parts;
+      newGeometry();
+    },
+    o(parts, unparsedArgs) {
+      object = unparsedArgs;
+      newGeometry();
+    },
+  };
+
+  const keywordRE = /(\w*)(?: )*(.*)/;
+  const lines = text.split('\n');
+  for (let lineNo = 0; lineNo < lines.length; ++lineNo) {
+    const line = lines[lineNo].trim();
+    if (line === '' || line.startsWith('#')) {
+      continue;
+    }
+    const m = keywordRE.exec(line);
+    if (!m) {
+      continue;
+    }
+    const [, keyword, unparsedArgs] = m;
+    const parts = line.split(/\s+/).slice(1);
+    const handler = keywords[keyword];
+    if (!handler) {
+      console.warn('unhandled keyword:', keyword);  // eslint-disable-line no-console
+      continue;
+    }
+    handler(parts, unparsedArgs);
+  }
+
+  // remove any arrays that have no entries.
+  for (const geometry of geometries) {
+    geometry.data = Object.fromEntries(
+        Object.entries(geometry.data).filter(([, array]) => array.length > 0));
+  }
+
+  return {
+    geometries,
+    materialLibs,
+  };
+}
+
+
+function init() {
+
+    fetch("FinalBaseMesh.obj") 
+        .then(response => response.text())
+        .then(text => {
+            modelData = parseOBJ(text);
+            checkAllLoaded();
+        });
+}
+
+function checkAllLoaded() {
+
+    if(modelData != null) {
+        initGL();
+        configScene();
+        draw();
+    }
+}
+    
+function initGL()
+{
+
+	var canvas = document.getElementById("glcanvas1");
+	
+	gl = getGL(canvas);
+	if(gl)
+	{
+        //Inicializa shaders
+ 		var vtxShSrc = document.getElementById("vertex-shader").text;
+		var fragShSrc = document.getElementById("frag-shader").text;
+
+        var vtxShader = createShader(gl, gl.VERTEX_SHADER, vtxShSrc);
+        var fragShader = createShader(gl, gl.FRAGMENT_SHADER, fragShSrc);
+        prog = createProgram(gl, vtxShader, fragShader);	
+        
+        gl.useProgram(prog);
+
+        //Inicializa área de desenho: viewport e cor de limpeza; limpa a tela
+        gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+        gl.clearColor(0, 0, 0, 1);
+        gl.enable( gl.BLEND );
+        gl.blendFunc( gl.SRC_ALPHA, gl.ONE_MINUS_SRC_ALPHA );
+        gl.enable(gl.DEPTH_TEST);
+        gl.enable(gl.CULL_FACE);
+
+    }
+}    
+var numVertices = 0;
+
+function configScene() {
+    const positions = modelData.geometries[0].data.position; 
+    
+    if (!positions) {
+        console.error("Não foram encontrados dados de posição no modelo!");
+        return;
+    }
+
+    numVertices = positions.length / 3;
+
+    var bufPtr = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufPtr);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+    var positionPtr = gl.getAttribLocation(prog, "position");
+    gl.enableVertexAttribArray(positionPtr);
+    gl.vertexAttribPointer(positionPtr, 3, gl.FLOAT, false, 0, 0);
+
+    var dfPtr = gl.getUniformLocation(prog, "df"); 
+    gl.uniform1f(dfPtr, df);
+}
+
+function draw() {
+    var matrotX = [
+        1.0, 0.0, 0.0, 0.0,
+        0.0, Math.cos(angle*Math.PI/180), -Math.sin(angle*Math.PI/180), 0.0,
+        0.0, Math.sin(angle*Math.PI/180),  Math.cos(angle*Math.PI/180), 0.0,
+        0.0, 0.0, 0.0, 1.0
+    ];
+
+    transfPtr = gl.getUniformLocation(prog, "transf");
+    gl.uniformMatrix4fv(transfPtr, false, matrotX);
+
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    gl.drawArrays(gl.TRIANGLES, 0, numVertices);
+
+    angle++;
+    requestAnimationFrame(draw);
+}
